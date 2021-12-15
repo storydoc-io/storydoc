f40ffeeeec71e2034d3a2ebbb3180de9224f34fe
Bram Stieperaere <bram@storydoc.io>
2021-12-15 17:21:21 +0100

    test github actions workflow

--- storydoc-backend/src/main/java/io/storydoc/StoryDocServerApplication.java
+++ storydoc-backend/src/main/java/io/storydoc/StoryDocServerApplication.java
@@ -3,7 +3,7 @@ package io.storydoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication_
 public class StoryDocServerApplication {
 
 	public static void main(String[] args) {
